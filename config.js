49cc58d2a9ad785e88d22011a8a8ba4e1dc9f21a
nilantha102306 <nilanthaweerapura95@gmail.com>
2025-01-28 10:16:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_28bdd35dc5ff6441cfae904ffb03a3aa76" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
